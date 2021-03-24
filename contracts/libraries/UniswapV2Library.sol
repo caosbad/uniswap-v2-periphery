9353ce4d5cabcef88d25775d91f6a119796f8a2e
Caos <caosbad@gmail.com>
2021-03-24 16:41:46 +0800

    update file

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'bfcdf7952c4cf2567d8f116cdb22d604f4c8e40ea2688efdb344d56910a189d8' // init code hash
             ))));
     }
 
